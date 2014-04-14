7fc3bee3933d56dab7a4055a4377082fad26010a
Venkatesh Nandakumar <venkatesh88@gmail.com>
2014-04-14 06:48:22 -0400

    New file, hello world

---
+++ tinder_like_all.pl
@@ -0,0 +1,18 @@
+#!/opt/local/bin/perl -W
+use strict;
+use warnings;
+use Data::Dump qw(dump);
+#use POSIX qw(strftime);
+#use Getopt::Long;
+#
+
+my $TINDER_TOKEN = "x";
+my $XAUTHTOKEN = "x";
+my $TINDER_ID = "x";
+
+die if !$TINDER_ID || !$XAUTHTOKEN || !$TINDER_ID;
+
+print "Hello all\n";
+
+
+exit 0;
