c51192b87775caf587e7f6187442c40aca85e559
Venkatesh Nandakumar <venkatesh88@gmail.com>
2014-04-18 00:45:43 -0400

    Added generic_curl() function using lwp

--- tinder_like_all.pl
+++ tinder_like_all.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Data::Dump qw(dump);
 use JSON;
+use LWP::UserAgent;
 #use POSIX qw(strftime);
 #use Getopt::Long;
 #
@@ -24,6 +25,12 @@ my $json_response;
 }
 my @match_ids = &strip_json($json_response);
 print $match_ids[1]. "\n";
+
+my %example_get_call = %{&generic_curl("1", "http://venkatesh.ca/",,)};
+
+if($example_get_call{'code'} eq "0") {
+    print $example_get_call{'response_body'}."\n";
+}
 
 sub strip_json {
     my $json_data = decode_json(shift);
@@ -50,10 +57,56 @@ sub strip_json {
 #   }
 # }
 #
-# sub generic_curl(is_get, query, header, body) {
-#       #do_magic
-#       return response_body;
-# }
+sub generic_curl() {
+    my $is_get = shift;
+    my $query = shift;
+    my $request_header = shift;
+    my $request_body = shift;
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(30);
+
+    my %response_headers = ();
+
+    if($is_get eq "1") {
+        my $req = HTTP::Request->new(GET => $query);
+        foreach (keys %{$request_header}) { 
+            $req->header($_ => $request_header->{$_});
+        }
+        my $resp = $ua->request($req);
+        if ($resp->is_success) {
+            my $message = $resp->decoded_content;
+            $response_headers{'response_body'} = $message;
+            $response_headers{'code'} = "0";
+            #print dump($resp->headers())."\n";
+        }
+        else {
+            print "HTTP GET error code: ", $resp->code, "\n";
+            print "HTTP GET error message: ", $resp->message, "\n";
+            $response_headers{'response_body'} = "";
+            $response_headers{'code'} = "404";
+        }
+    } else {
+        #post
+        my $req = HTTP::Request->new(POST => $query);
+        foreach (keys %{$request_header}) { 
+            $req->header($_ => $request_header->{$_});
+        }
+        $req->content($request_body);
+        my $resp = $ua->request($req);
+        if ($resp->is_success) {
+            my $message = $resp->decoded_content;
+            $response_headers{'response_body'} = $message;
+            $response_headers{'code'} = "0";
+        }
+        else {
+            print "HTTP POST error code: ", $resp->code, "\n";
+            print "HTTP POST error message: ", $resp->message, "\n";
+            $response_headers{'response_body'} = "";
+            $response_headers{'code'} = "404";
+        }
+    }
+    return \%response_headers;
+}
 #
 # #set_like_curl and get_forty both use generic_curl
 #
